4d02fed784a8056f19edca631ff78c946f7fcc70
surajrajput84 <surajrajput8459@gmail.com>
2025-09-04 12:10:01 +0530

    without try catch
    ass 5

---
+++ Addd5.java
@@ -0,0 +1,15 @@
+public class Addd5
+{
+	public static void main(String args[])
+	{
+		int a=10,b=0;
+		int e=a-b;
+		System.out.println("the Substraction is : "+e);
+
+		int c=a/b;
+		System.out.println("the division is : "+c);
+		int d=a+b;
+		System.out.println("the Addition is : "+d);
+
+	}
+}
